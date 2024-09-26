bf3b3bf126b26546845e09557d29fcc6c30cf589
Oscar <a18macaos@iesgrancapitan.org>
2024-09-26 10:02:04 +0200

    Unidad2

---
+++ Plantilla html/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Plantilla</title>
+</head>
+<body>
+    <div>
+        <h1><?php echo "Óscar Martín-Castaño Carrillo"?></h1>
+        <h2><?php echo "DWES"?></h2>
+        <ul>
+            <li>Mail: <?php echo "2005omcc@gmail.com"?></li>
+            <li>Phone: <?php echo "mi numero"?></li>
+            <li>Mail: <?php echo "2005omcc@gmail.com"?></li>
+            <li>Mail: <?php echo "2005omcc@gmail.com"?></li>
+            <li>Mail: <?php echo "2005omcc@gmail.com"?></li>
+        </ul>
+    </div>
+</body>
+</html>
